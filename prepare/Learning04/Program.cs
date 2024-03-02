4134c8ddec6adb25f09864182ac8299828abbef7
Haein05 <lee21025@byui.edu>
2024-03-02 12:16:24 -0700

    Mar 2

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,3 +7,30 @@ class Program
         Console.WriteLine("Hello Learning04 World!");
     }
 }
+
+//Person
+class Person {
+    private string id;
+    private string name;
+
+    public Person(string id, string name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void display()
+    {
+        System.Console.WriteLine($"{name}: {id}");
+    }
+}
+// Student
+class Student : Person{
+    private string major;
+    public Student(string id, string name, string majoir) : base(id, name) {
+        this.major = majoir;
+    }
+    public void display()
+    {
+        System.Console.WriteLine($"{name}: {id}");
+    }
+}
---
+++ prove/Develop04/breathing.cs
@@ -0,0 +1,58 @@
+class Breathing:Activity{
+
+    public Breathing(){
+        activity = "Breathing";
+        description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+
+    }
+
+    public void do_activity(){
+        DateTime startTime = DateTime.Now;
+
+        while (DateTime.Now < startTime.AddSeconds(duration))
+        {
+            Console.Write("Breathe in...");
+            Thread.Sleep(1000);
+            Console.Write("5");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("4");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("3");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("2");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("1");
+            Console.Write("\b \b");
+
+            Console.Write("Breathe out...");
+
+            Thread.Sleep(1000);
+            Console.Write("5");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("4");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("3");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("2");
+            Console.Write("\b \b");
+
+            Thread.Sleep(1000);
+            Console.Write("1");
+            Console.Write("\b \b");
+        }
+    }
+}
