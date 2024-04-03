a5bf12fa79eff4b3c18926292d6c2def617bf58d
Haein05 <lee21025@byui.edu>
2024-04-03 15:41:10 -0600

    Apr 3

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -3,7 +3,22 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation4 World!");
+    {  
+        List<Activity> activity = new List<Activity>();
+
+        RunningActivity  runningActivity = new RunningActivity(DateTime.Now,45, 7.5f);
+        SwimmingActivity swimmingActivity = new SwimmingActivity (DateTime.Now,60, 20);
+        CyclingActivity cyclingActivity = new CyclingActivity (DateTime.Now,120, 50);
+        
+        activity.Add(runningActivity);
+        activity.Add(swimmingActivity);
+        activity.Add(cyclingActivity);
+
+        foreach (Activity a in activity)
+        {
+            Console.WriteLine(a.GetSummary());
+        }
+
+        
     }
 }
